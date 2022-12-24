3084e3851284c7e2d865667f2a93d7eef3eff4c3
othea32 <othea32@gmail.com>
2022-12-24 15:33:50 -0800

    first commit

---
+++ client/script.js
@@ -0,0 +1,84 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat-container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if(index < text.length) {
+      element.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe (isAi, value, unique) {
+  return(
+    `
+      <div class="wrapper ${isAi && 'ai'}>
+        <div class="chat>
+          <div classname="profile">
+            <img
+              src="${isAi ? bot : user}"
+              alt="${isAi ? 'bot' : 'user'}"
+            />
+          </div>
+          <div class="message" id=${uniqueId}>${value}</div>
+        </div>
+      </div>
+    `
+  )
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, '', uniqueId);
+  
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const message = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
