1b4e7eb0af2b94cc088da4e5ee12cbc21c388382
othea32 <othea32@gmail.com>
2022-12-25 00:03:06 -0800

    update the server url

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-1gml.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
